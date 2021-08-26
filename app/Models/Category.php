763cd97644023fda35c75e9cde79d42b6a570010
Olivier <olivier@ogasphere.com>
2021-08-26 08:53:32 +0200

    modif model category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
    protected $fillable = ['name', 'status'];
+   protected $visible = ['name'];
    protected $hidden = ['id', 'created_at', 'updated_at'];
 }
 ?>
